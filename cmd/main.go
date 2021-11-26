8ba5db934809b8546a3a0a39e0a99a54042215af
walkure <walkure@3pf.jp>
2021-11-27 03:31:42 +0900

    統合した

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,55 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"sync"
 
+	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 	"github.com/slack-go/slack/socketmode"
 
 	common "github.com/walkure/aggrechans"
 )
+
+var AGG_CHAN_ID = os.Getenv("AGGREGATE_CHANNEL_ID")
+
+func createSlackSocketClient() (*slack.Client, *socketmode.Client, error) {
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	if appToken == "" {
+		return nil, nil, errors.New("SLACK_APP_TOKEN must be set")
+	}
+
+	if !strings.HasPrefix(appToken, "xapp-") {
+		return nil, nil, errors.New("SLACK_APP_TOKEN must have the prefix \"xapp-\"")
+	}
+
+	botToken := os.Getenv("SLACK_BOT_TOKEN")
+	if botToken == "" {
+		return nil, nil, errors.New("SLACK_BOT_TOKEN must be set")
+	}
+
+	if !strings.HasPrefix(botToken, "xoxb-") {
+		return nil, nil, errors.New("SLACK_BOT_TOKEN must have the prefix \"xoxb-\"")
+	}
+
+	api := slack.New(
+		botToken,
+		slack.OptionAppLevelToken(appToken),
+		//slack.OptionDebug(true),
+		//slack.OptionLog(log.New(os.Stdout, "api: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	client := socketmode.New(
+		api,
+		//socketmode.OptionDebug(true),
+		//socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	return api, client, nil
+}
 
 func main() {
 	api, client, err := createSlackSocketClient()
